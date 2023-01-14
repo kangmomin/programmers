1d7d2ed426ab86a65e36b1b68de2f12b1e44cda0
koldin108902 <alex108902@naver.com>
2023-01-15 00:30:27 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] solution1 = solution.solution(	1, 2, 3, 4);
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+    }
+}
